eab66bdbd804d513ed301060c83f4cb25313ca19
Gutyerrez <srgutyerrez@gmail.com>
2020-03-17 02:38:24 -0300

    Create BattleState.java

---
+++ src/main/java/com/redecommunity/factions/entity/faction/enums/BattleState.java
@@ -0,0 +1,11 @@
+package com.redecommunity.factions.entity.faction.enums;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public enum BattleState {
+    REQUESTED,
+    STARTING,
+    STARTED,
+    ENDED;
+}
