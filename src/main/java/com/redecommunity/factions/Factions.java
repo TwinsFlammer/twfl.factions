224b8514242f78dccaf32c8bd4ee44d24e47bcb8
Gutyerrez <srgutyerrez@gmail.com>
2020-03-17 02:39:41 -0300

    Create Factions.java

---
+++ src/main/java/com/redecommunity/factions/Factions.java
@@ -0,0 +1,24 @@
+package com.redecommunity.factions;
+
+import com.redecommunity.api.spigot.CommunityPlugin;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class Factions extends CommunityPlugin {
+    private static Factions instance;
+
+    public static Factions getInstance() {
+        return Factions.instance;
+    }
+
+    @Override
+    public void onEnablePlugin() {
+
+    }
+
+    @Override
+    public void onDisablePlugin() {
+
+    }
+}
