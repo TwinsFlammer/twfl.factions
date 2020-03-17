5be8286f9e6ac650b1ea8ee30595b0b6544671c1
Gutyerrez <srgutyerrez@gmail.com>
2020-03-17 02:38:39 -0300

    Create MemberRole.java

---
+++ src/main/java/com/redecommunity/factions/entity/member/enums/MemberRole.java
@@ -0,0 +1,15 @@
+package com.redecommunity.factions.entity.member.enums;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public enum MemberRole {
+    NONE,
+    RECRUIT,
+    MEMBER,
+    CAPTAIN,
+    LEADER,
+    NEUTRAL,
+    ALLY,
+    ENEMY;
+}
