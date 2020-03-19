76179f4986a0dcc6a12d40bbde948ac75960ccd5
Gutyerrez <srgutyerrez@gmail.com>
2020-03-19 05:24:44 -0300

    Update Permission.java

--- src/main/java/com/redecommunity/factions/permission/data/Permission.java
+++ src/main/java/com/redecommunity/factions/permission/data/Permission.java
@@ -3,12 +3,12 @@ package com.redecommunity.factions.permission.data;
 import com.redecommunity.factions.permission.enums.PermissionType;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
+import java.util.HashMap;
 
 /**
  * Created by @SrGutyerrez
  */
 @RequiredArgsConstructor
 public class Permission {
-    private final List<PermissionType> permissions;
+    private final HashMap<PermissionType, Boolean> permissions;
 }
