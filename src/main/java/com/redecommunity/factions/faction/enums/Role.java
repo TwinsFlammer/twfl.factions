5dec7e1ac8260bd07833caff89e764de859a0148
Gutyerrez <srgutyerrez@gmail.com>
2020-03-19 05:25:25 -0300

    Update Role.java

--- src/main/java/com/redecommunity/factions/faction/enums/Role.java
+++ src/main/java/com/redecommunity/factions/faction/enums/Role.java
@@ -1,11 +1,47 @@
 package com.redecommunity.factions.faction.enums;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * Created by @SrGutyerrez
  */
+@RequiredArgsConstructor
 public enum Role {
-    RECRUIT,
-    MEMBER,
-    CAPTAIN,
-    LEADER;
+    RECRUIT(
+            "recruta",
+            "Recruta",
+            "-"
+    ),
+    MEMBER(
+            "membro",
+            "Membro",
+            "+"
+    ),
+    OFFICER(
+            "capitão",
+            "Capitão",
+            "*"
+    ),
+    LEADER(
+            "líder",
+            "Líder",
+            "#"
+    ),
+    NEUTRAL(
+            "aliada",
+            "Aliada",
+            null
+    ),
+    ENEMY(
+            "aliada",
+            "Aliada",
+            null
+    ),
+    ALLY(
+            "aliada",
+            "Aliada",
+            null
+    );
+
+    private final String name, displayName, prefix;
 }
