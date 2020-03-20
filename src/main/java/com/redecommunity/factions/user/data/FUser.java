8f7fb1b991a1de3ebe590822272fc9041b0e915a
Gutyerrez <srgutyerrez@gmail.com>
2020-03-20 11:49:46 -0300

    Update FUser.java

--- src/main/java/com/redecommunity/factions/user/data/FUser.java
+++ src/main/java/com/redecommunity/factions/user/data/FUser.java
@@ -1,16 +1,29 @@
 package com.redecommunity.factions.user.data;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.redecommunity.api.spigot.inventory.item.CustomItem;
 import com.redecommunity.api.spigot.user.data.SpigotUser;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.skin.data.Skin;
+import com.redecommunity.common.shared.util.Helper;
+import com.redecommunity.factions.Factions;
 import com.redecommunity.factions.faction.data.Faction;
 import com.redecommunity.factions.faction.enums.Role;
 import com.redecommunity.factions.faction.manager.FactionManager;
+import com.redecommunity.factions.land.data.Land;
 import com.redecommunity.factions.permission.data.Permission;
+import com.redecommunity.factions.permission.enums.PermissionType;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.Warning;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -42,10 +55,32 @@ public class FUser extends SpigotUser {
     private final List<Integer> invites = Lists.newArrayList();
 
     @Getter
-    private final List<Permission> permissions = Lists.newArrayList();
+    private final Permission permission;
+
+    @Getter
+    private Integer warWins;
 
     public FUser(User user) {
         super(user);
+
+        // create this value
+        this.permission = new Permission(Maps.newHashMap());
+    }
+
+    public void sendMessage(String message) {
+        if (this.isOnline())
+            this.getPlayer().sendMessage(Helper.colorize(message));
+    }
+
+    public void tryClaim(Faction faction, Chunk chunk) {
+        Land land = Factions.getLandFactory().getLand(chunk);
+
+        if (land == null) {
+            this.sendMessage("§cOcorreu um erro ao tentar comprar esta terra");
+            return;
+        }
+
+        // continuar
     }
 
     public List<Faction> getInvites() {
@@ -62,6 +97,29 @@ public class FUser extends SpigotUser {
 
     public Faction getFaction() {
         return FactionManager.getFaction(this.factionId);
+    }
+
+    public CustomItem getSkull() {
+        Skin skin = this.getSkin();
+        Role role = this.getRole();
+
+
+        return new CustomItem(Material.SKULL_ITEM)
+                .name(this.getHighestGroup().getFancyPrefix() + this.getDisplayName())
+                .data(3)
+                .owner(skin == null ? this.getDisplayName() : skin.getValue())
+                .lore(
+                        "§fPoder: §7" + this.getPowerRounded() + "/" + this.getPowerMaxRounded(),
+                        "§fCoins: §7" + this.getMoneyFormatted(),
+                        "§fCargo: §7" + role.getPrefix() + role.getDisplayName(),
+                        "§fKDR: §7" + this.getKDRFormatted(),
+                        "§fAbates: §7" + this.getKillsFormatted(),
+                        "§fMortes: §7" + this.getDeathsFormatted(),
+                        "§fGuerras ganhas: §7" + this.warWins,
+                        "§fStatus §7" + (this.isOnline() ? "§aOnline" : "§cOffline"),
+                        "§fÚltimo login: §7" + this.getLastLoginFormatted()
+                )
+                .hideAttributes();
     }
 
     public String getFactionName() {
@@ -82,7 +140,7 @@ public class FUser extends SpigotUser {
         );
     }
 
-    public String getKDR() {
+    public String getKDRFormatted() {
         int kills = this.getKills();
         int deaths = this.getDeaths() == 0 ? 1 : this.getDeaths();
 
@@ -91,12 +149,67 @@ public class FUser extends SpigotUser {
         return decimalFormat.format(kills / deaths);
     }
 
+    public String getKillsFormatted() {
+        return String.format(
+                "§8[Inimigo §7%d§8 Neutro §7%d§8 Civil §7%d§8]",
+                this.killsEnemy,
+                this.killsNeutral,
+                this.killsCivilian
+        );
+    }
+
+    public String getDeathsFormatted() {
+        return String.format(
+                "§8[Inimigo §7%d§8 Neutro §7%d§8 Civil §7%d§8]",
+                this.deathsEnemy,
+                this.deathsNeutral,
+                this.deathsCivilian
+        );
+    }
+
+    public String getMoneyFormatted() {
+        return null;
+    }
+
+    public String getLastLoginFormatted(){
+        Date date = new Date(this.lastLogin);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        return simpleDateFormat.format(date);
+    }
+
+    public double getKDR() {
+        int kills = this.getKills();
+        int deaths = this.getDeaths() == 0 ? 1 : this.getDeaths();
+
+        return kills / deaths;
+    }
+
     public Integer getKills() {
         return this.killsCivilian + this.killsNeutral + this.killsEnemy;
     }
 
     public Integer getDeaths() {
         return this.deathsCivilian + this.deathsNeutral + this.deathsEnemy;
+    }
+
+    public Integer getPowerRounded() {
+        return (int) Math.round(this.power);
+    }
+
+    public Integer getPowerMaxRounded() {
+        return (int) Math.round(this.powerMax);
+    }
+
+    @Warning
+    public Double getMoney() {
+        return null;
+    }
+
+    @Warning
+    public void withdraw(Double value) {
+        // TODO not implemented-yet
     }
 
     public Boolean isMapAutoUpdating() {
@@ -117,5 +230,25 @@ public class FUser extends SpigotUser {
 
     public Boolean isLeader() {
         return this.role == Role.LEADER;
+    }
+
+    public Boolean isOfficer() {
+        return this.isLeader() || this.role == Role.OFFICER;
+    }
+
+    public Boolean isMember() {
+        return this.isLeader() || this.isOfficer() || this.role == Role.MEMBER;
+    }
+
+    public Boolean hasPermission(PermissionType permissionType) {
+        return this.permission.has(permissionType) || permissionType.getDefault(this.role);
+    }
+
+    public Boolean isOnline() {
+        return this.getPlayer() != null;
+    }
+
+    public Boolean hasFaction() {
+        return !(this.factionId.equals(Factions.ID_NONE) && this.factionId.equals(Factions.ID_SAFEZONE) && this.factionId.equals(Factions.ID_WARZONE));
     }
 }
