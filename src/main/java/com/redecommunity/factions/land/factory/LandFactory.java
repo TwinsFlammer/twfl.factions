c27819acda3090593615a6d49eaed7a90f657383
Gutyerrez <srgutyerrez@gmail.com>
2020-03-20 12:55:28 -0300

    Update LandFactory.java

--- src/main/java/com/redecommunity/factions/land/factory/LandFactory.java
+++ src/main/java/com/redecommunity/factions/land/factory/LandFactory.java
@@ -1,8 +1,11 @@
 package com.redecommunity.factions.land.factory;
 
+import com.redecommunity.factions.Factions;
 import com.redecommunity.factions.faction.manager.FactionManager;
 import com.redecommunity.factions.land.data.Land;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.World;
 
 /**
  * Created by @SrGutyerrez
@@ -21,8 +24,32 @@ public class LandFactory<L extends Land> {
                         .filter(land1 -> land1.isSimilar(chunk))
                         .findFirst()
                 )
-                .orElse(null);
+                .orElse(
+                        new Land(
+                                0,
+                                Factions.ID_NONE,
+                                chunk.getWorld().getName(),
+                                chunk.getX(),
+                                chunk.getZ(),
+                                System.currentTimeMillis(),
+                                -1L,
+                                null,
+                                false
+                        )
+                );
 
         return (L) land;
+    }
+
+    public L getLand(World world, Integer x, Integer z) {
+        Chunk chunk = world.getChunkAt(x, z);
+
+        return this.getLand(chunk);
+    }
+
+    public L getLand(String worldName, Integer x, Integer z) {
+        World world = Bukkit.getWorld(worldName);
+
+        return this.getLand(world, x, z);
     }
 }
