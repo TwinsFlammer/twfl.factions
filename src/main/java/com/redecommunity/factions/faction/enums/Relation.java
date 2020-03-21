ecdfae9df85ef8f1b8bcb04872fcec811f97db95
Gutyerrez <srgutyerrez@gmail.com>
2020-03-21 09:43:22 -0300

    Update Relation.java

--- src/main/java/com/redecommunity/factions/faction/enums/Relation.java
+++ src/main/java/com/redecommunity/factions/faction/enums/Relation.java
@@ -1,10 +1,41 @@
 package com.redecommunity.factions.faction.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+
 /**
  * Created by @SrGutyerrez
  */
+@RequiredArgsConstructor
 public enum Relation {
-    NEUTRAL,
-    ENEMY,
-    ALLY;
+    NEUTRAL(
+            "neutra",
+            "Neutra",
+            "Neutro",
+            Color.WHITE,
+            ChatColor.WHITE
+    ),
+    ENEMY(
+            "inimiga",
+            "Inimiga",
+            "Inimigo",
+            Color.RED,
+            ChatColor.RED
+    ),
+    ALLY(
+            "aliada",
+            "Aliada",
+            "Aliado",
+            Color.GREEN,
+            ChatColor.GREEN
+    );
+
+    @Getter
+    private final String name, displayName, sampleName;
+    @Getter
+    private final Color color;
+    @Getter
+    private final ChatColor chatColor;
 }
