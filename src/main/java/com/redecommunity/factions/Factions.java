e688dac084fede7d43f03fe8e5d3c285f37f2c02
Gutyerrez <srgutyerrez@gmail.com>
2020-03-22 15:04:43 -0300

    Update Factions.java

--- src/main/java/com/redecommunity/factions/Factions.java
+++ src/main/java/com/redecommunity/factions/Factions.java
@@ -3,6 +3,8 @@ package com.redecommunity.factions;
 import com.redecommunity.api.spigot.CommunityPlugin;
 import com.redecommunity.factions.land.data.Land;
 import com.redecommunity.factions.land.factory.LandFactory;
+import com.redecommunity.factions.user.data.FUser;
+import com.redecommunity.factions.user.factory.FUserFactory;
 import lombok.Getter;
 
 /**
@@ -21,6 +23,7 @@ public class Factions extends CommunityPlugin {
     public static final Integer ID_WARZONE = 3;
 
     private static final LandFactory<? extends Land> LAND_FACTORY = new LandFactory<>();
+    private static final FUserFactory<? extends FUser> F_USER_FACTORY = new FUserFactory<>();
 
     @Override
     public void onEnablePlugin() {
@@ -33,5 +36,9 @@ public class Factions extends CommunityPlugin {
 
     public static LandFactory<? extends Land> getLandFactory() {
         return Factions.LAND_FACTORY;
+    }
+
+    public static FUserFactory<? extends FUser> getFUserFactory() {
+        return Factions.F_USER_FACTORY;
     }
 }
