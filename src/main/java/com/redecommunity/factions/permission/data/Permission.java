d8b2b789233445511583226b2865d06a01965436
Gutyerrez <srgutyerrez@gmail.com>
2020-03-22 15:03:41 -0300

    Update Permission.java

--- src/main/java/com/redecommunity/factions/permission/data/Permission.java
+++ src/main/java/com/redecommunity/factions/permission/data/Permission.java
@@ -1,8 +1,11 @@
 package com.redecommunity.factions.permission.data;
 
+import com.google.common.collect.Maps;
 import com.redecommunity.factions.permission.enums.PermissionType;
 import lombok.RequiredArgsConstructor;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 /**
@@ -12,10 +15,32 @@ import java.util.HashMap;
 public class Permission {
     private final HashMap<PermissionType, Boolean> permissions;
 
+    public Boolean addPermission(PermissionType permissionType, Boolean value) {
+        return this.permissions.put(
+                permissionType,
+                value
+        );
+    }
+
     public Boolean has(PermissionType permissionType) {
         return this.permissions.getOrDefault(
                 permissionType,
                 false
+        );
+    }
+
+    public static Permission toPermission(ResultSet resultSet) throws SQLException {
+        HashMap<PermissionType, Boolean> permissions = Maps.newHashMap();
+
+        for (PermissionType permissionType : PermissionType.values()) {
+            permissions.put(
+                    permissionType,
+                    resultSet.getBoolean(permissionType.getColumnName())
+            );
+        }
+
+        return new Permission(
+                permissions
         );
     }
 }
