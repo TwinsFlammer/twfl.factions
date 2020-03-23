2f68f294b3a93c78fc6f52eb45e2a93c5a1b3de9
Gutyerrez <srgutyerrez@gmail.com>
2020-03-23 14:13:33 -0300

    Update Battle.java

--- src/main/java/com/redecommunity/factions/battle/data/Battle.java
+++ src/main/java/com/redecommunity/factions/battle/data/Battle.java
@@ -1,7 +1,30 @@
 package com.redecommunity.factions.battle.data;
 
+import com.redecommunity.factions.battle.enums.BattleState;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+
 /**
  * Created by @SrGutyerrez
  */
+@AllArgsConstructor
 public class Battle {
+    @Getter
+    private final Integer id, challengerId, challengedId;
+    @Getter
+    private final Long time;
+
+    @Getter
+    @Setter
+    private Long duration;
+
+    @Getter
+    @Setter
+    private BattleState battleState;
+
+    @Getter
+    private final HashMap<Integer, Integer> kills, deaths;
 }
