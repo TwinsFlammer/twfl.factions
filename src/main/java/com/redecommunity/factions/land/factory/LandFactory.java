9fedad0471b1377904d613c88880dcdb3ef6b6b4
Gutyerrez <srgutyerrez@gmail.com>
2020-03-23 10:37:04 -0300

    Update LandFactory.java

--- src/main/java/com/redecommunity/factions/land/factory/LandFactory.java
+++ src/main/java/com/redecommunity/factions/land/factory/LandFactory.java
@@ -41,6 +41,26 @@ public class LandFactory<L extends Land> {
         return (L) land;
     }
 
+    public L getLand(Integer id) {
+        Land land = FactionManager.getFactions()
+                .stream()
+                .filter(faction1 -> faction1.getLands()
+                        .stream()
+                        .anyMatch(land1 -> land1.getId().equals(id))
+                )
+                .findFirst()
+                .flatMap(faction1 -> faction1.getLands()
+                        .stream()
+                        .filter(land1 -> land1.getId().equals(id))
+                        .findFirst()
+                )
+                .orElse(
+                        null
+                );
+
+        return (L) land;
+    }
+
     public L getLand(World world, Integer x, Integer z) {
         Chunk chunk = world.getChunkAt(x, z);
 
