76da66dd1a5e0f76aef79c675a16687c094082a3
Gutyerrez <srgutyerrez@gmail.com>
2020-03-26 15:17:38 -0300

    Update Battle.java

--- src/main/java/com/redecommunity/factions/battle/data/Battle.java
+++ src/main/java/com/redecommunity/factions/battle/data/Battle.java
@@ -1,10 +1,13 @@
 package com.redecommunity.factions.battle.data;
 
-import com.redecommunity.factions.battle.enums.BattleState;
+import com.google.common.collect.Maps;
+import com.redecommunity.factions.battle.enums.State;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 
 /**
@@ -23,8 +26,66 @@ public class Battle {
 
     @Getter
     @Setter
-    private BattleState battleState;
+    private State state;
 
     @Getter
     private final HashMap<Integer, Integer> kills, deaths;
+
+    public Integer getChallengerKills() {
+        return this.kills.getOrDefault(this.challengerId, 0);
+    }
+
+    public Integer getChallengedKills() {
+        return this.kills.getOrDefault(this.challengedId, 0);
+    }
+
+    public Integer getChallengerDeaths() {
+        return this.deaths.getOrDefault(this.challengerId, 0);
+    }
+
+    public Integer getChallengedDeaths() {
+        return this.deaths.getOrDefault(this.challengedId, 0);
+    }
+
+    public static Battle toBattle(ResultSet resultSet) throws SQLException {
+        Integer challengerId = resultSet.getInt("challenger_id"),
+                challengedId = resultSet.getInt("challenged_id");
+
+        HashMap<Integer, Integer> kills = Maps.newHashMap(), deaths = Maps.newHashMap();
+
+        kills.put(
+                challengerId,
+                resultSet.getInt("challenger_kills")
+        );
+
+        kills.put(
+                challengedId,
+                resultSet.getInt("challenged_kills")
+        );
+
+        deaths.put(
+                challengerId,
+                resultSet.getInt("challenger_deaths")
+        );
+
+        deaths.put(
+                challengedId,
+                resultSet.getInt("challenged_deaths")
+        );
+
+        State state = State.valueOf(
+                resultSet.getString("state")
+        );
+
+        return new Battle(
+                resultSet.getInt("id"),
+                resultSet.getInt("challenger_id"),
+                resultSet.getInt("challenged_id"),
+                resultSet.getLong("time"),
+                resultSet.getLong("duration"),
+                state,
+                kills,
+                deaths
+        );
+    }
 }
