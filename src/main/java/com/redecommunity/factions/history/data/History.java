d0991cf0d8aafaa8f6791bd57d7c1589332f024a
Gutyerrez <srgutyerrez@gmail.com>
2020-03-30 09:54:08 -0300

    Update History.java

--- src/main/java/com/redecommunity/factions/history/data/History.java
+++ src/main/java/com/redecommunity/factions/history/data/History.java
@@ -1,7 +1,12 @@
 package com.redecommunity.factions.history.data;
 
+import com.redecommunity.factions.faction.data.Faction;
+import com.redecommunity.factions.faction.manager.FactionManager;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by @SrGutyerrez
@@ -11,4 +16,23 @@ import lombok.RequiredArgsConstructor;
 public class History {
     private final String oldTag, newTag, oldName, newName;
     private final Long time;
+
+    public Integer getFactionId() {
+        return FactionManager.getFactions()
+                .stream()
+                .filter(faction -> faction.getHistory().contains(this))
+                .map(Faction::getId)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static History toHistory(ResultSet resultSet) throws SQLException {
+        return new History(
+                resultSet.getString("old_tag"),
+                resultSet.getString("new_tag"),
+                resultSet.getString("old_name"),
+                resultSet.getString("new_name"),
+                resultSet.getLong("time")
+        );
+    }
 }
