f36b41ac786c5ffee4d173d8b34ed6e23b2044af
Gutyerrez <srgutyerrez@gmail.com>
2020-03-30 09:53:25 -0300

    Update Generator.java

--- src/main/java/com/redecommunity/factions/generator/data/Generator.java
+++ src/main/java/com/redecommunity/factions/generator/data/Generator.java
@@ -1,7 +1,12 @@
 package com.redecommunity.factions.generator.data;
 
+import lombok.AllArgsConstructor;
+
 /**
  * Created by @SrGutyerrez
  */
+@AllArgsConstructor
 public class Generator {
+    private final Integer id;
+    
 }
