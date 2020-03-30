6359abcc0009b5eb80ccc8e401a196489201b931
Gutyerrez <srgutyerrez@gmail.com>
2020-03-30 11:20:08 -0300

    Update FactionCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/FactionCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/FactionCommand.java
@@ -4,6 +4,10 @@ import com.redecommunity.api.spigot.commands.CustomCommand;
 import com.redecommunity.api.spigot.commands.enums.CommandRestriction;
 import com.redecommunity.common.shared.permissions.group.GroupNames;
 import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.factions.faction.data.Faction;
+import com.redecommunity.factions.faction.manager.FactionManager;
+import com.redecommunity.factions.user.data.FUser;
+import com.redecommunity.factions.user.manager.FUserManager;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -19,7 +23,29 @@ public class FactionCommand extends CustomCommand {
     }
 
     @Override
-    public void onCommand(CommandSender commandSender, User user, String[] strings) {
+    public void onCommand(CommandSender commandSender, User user, String[] args) {
+        String arg = args[0];
 
+        Faction faction = FactionManager.getFaction(arg);
+
+        if (faction != null) {
+            // TODO not implemented yet
+            return;
+        }
+
+        FUser fUser = FUserManager.getFUser(arg);
+
+        if (fUser != null) {
+            // TODO not implemented yet
+            return;
+        }
+
+        if (arg.length() <= 3) {
+            // TODO not implemented yet
+            return;
+        } else {
+            // TODO not implemented yet
+            return;
+        }
     }
 }
