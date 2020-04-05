66984ec8081d339cb690e5f166b242905dcc6e6b
Gutyerrez <srgutyerrez@gmail.com>
2020-04-05 15:27:11 -0300

    Update FactionAdminCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/argument/FactionAdminCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/argument/FactionAdminCommand.java
@@ -1,4 +1,38 @@
 package com.redecommunity.factions.faction.command.argument;
 
-public class FactionAdminCommand {
+import com.google.common.collect.Maps;
+import com.redecommunity.common.shared.permissions.group.GroupNames;
+import com.redecommunity.factions.user.dao.FUserDAO;
+import com.redecommunity.factions.user.data.FUser;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+
+public class FactionAdminCommand extends AbstractFactionArgumentCommand {
+    public FactionAdminCommand() {
+        super(
+                0,
+                "admin",
+                GroupNames.MANAGER
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        fUser.setOverriding(!fUser.isOverriding());
+
+        FUserDAO fUserDAO = new FUserDAO();
+
+        HashMap<String, Object> keys = Maps.newHashMap();
+
+        keys.put("overriding", fUser.isOverriding());
+
+        fUserDAO.update(
+                keys,
+                "user_id",
+                fUser.getId()
+        );
+
+        commandSender.sendMessage("§aModo administrativo " + (fUser.isOverriding() ? "ativado." : "desativado"));
+    }
 }
