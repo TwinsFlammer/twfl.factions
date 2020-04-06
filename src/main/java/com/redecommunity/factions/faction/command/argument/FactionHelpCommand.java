01575f25ee304a31a7b4718052fab5348d3d816e
Gutyerrez <srgutyerrez@gmail.com>
2020-04-06 15:38:34 -0300

    Create FactionHelpCommand.java

---
+++ src/main/java/com/redecommunity/factions/faction/command/argument/FactionHelpCommand.java
@@ -0,0 +1,23 @@
+package com.redecommunity.factions.faction.command.argument;
+
+import com.redecommunity.factions.user.data.FUser;
+import com.redecommunity.factions.util.Messages;
+import org.bukkit.command.CommandSender;
+
+public class FactionHelpCommand extends AbstractFactionArgumentCommand {
+    public FactionHelpCommand() {
+        super(
+                0,
+                "ajuda"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        if (fUser.hasFaction()) {
+            commandSender.sendMessage(Messages.DEFAULT_HELP_MESSAGE_WITH_FACTION);
+        } else {
+            commandSender.sendMessage(Messages.DEFAULT_HELP_MESSAGE_WITHOUT_FACTION);
+        }
+    }
+}
