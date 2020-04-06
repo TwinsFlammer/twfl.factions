64537a8b3aaa974d75ceb50dfcbc6324bf2198b6
Gutyerrez <srgutyerrez@gmail.com>
2020-04-06 15:39:34 -0300

    Update Messages.java

--- src/main/java/com/redecommunity/factions/util/Messages.java
+++ src/main/java/com/redecommunity/factions/util/Messages.java
@@ -2,4 +2,54 @@ package com.redecommunity.factions.util;
 
 public class Messages {
     public static final String FACTION_NEEDED = "§cVocê não está em uma facção.";
+    public static final String USER_NOT_EXISTS = "§cEste usuário não existe";
+    public static final String INST_IN_YOUR_FACTION = "§cEste usuário não pertence a sua facção.";
+    public static final String[] DEFAULT_HELP_MESSAGE_WITHOUT_FACTION = {
+            "",
+            "§2Comandos disponíveis:",
+            "",
+            "§a/f aceitar convite <tag> §8- §7Utilize para aceitar convites de facções!",
+            "§a/f base §8- §7Utilize para ir até a base de sua facção.",
+            "§a/f convites §8- §7Utilize para ver seus convites de facção.",
+            "§a/f criar <tag> <nome> §8- §7Crie sua própria facção.",
+            "§a/f geradores §8- §7Utilize para ver os geradores da sua facção.",
+            "§a/f info <tag> §8- §7Utilize para ver informações sobre uma facção.",
+            "§a/f listar §8- §7Utilize para ver todas as facções online.",
+            "§a/f mapa §8- §7Utilize para ver o mapa.",
+            "§a/f membros <tag> §8- §7Utilize para listar membros de uma facção.",
+            "§a/f perfil <jogador> §8- §7Mostrar as informações de um jogador.",
+            "§a/f ranking §8- §7Veja os Rankings com as melhores facções do servidor.",
+            "§a/f setspawn §8- §7Define a local onde os mobs vão nascer.",
+            "§a/f verterras §8- §7Mostrar delimitações das terras."
+    };
+    public static final String[] DEFAULT_HELP_MESSAGE_WITH_FACTION = {
+            "",
+            "§2Comandos disponíveis:",
+            "",
+            "§a/f abandonar §8- §7Abandona terras.",
+            "§a/f base §8- §7Utilize para ir até  a base de sua facção.",
+            "§a/f convidar <jogador> §8- §7Utilize para enviar convites de facções.",
+            "§a/f convites §8- §7Utilize para ver seus convites de facção.",
+            "§a/f desfazer §8- §7Utilize para terminar sua facção.",
+            "§a/f dominar §8- §7Utilize para dominar uma terra.",
+            "§a/f expulsar <jogador> §8- §7Utilize para expulsar membros de sua facção.",
+            "§a/f fixchunk §8- §7Corrige o bug da chunk.",
+            "§a/f geradores §8- §7Utilize para ver os geradores da sua facção.",
+            "§a/f info <tag> §8- §7Utilize para ver informações sobre uma facção.",
+            "§a/f listar §8- §7Utilize para listar todas as facções online.",
+            "§a/f mapa §8- §7Utilize para ver o mapa.",
+            "§a/f membros <tag> §8- §7Utilize para listar membros de uma facção.",
+            "§a/f perfil <jogador> §8- §7Mostrar informações de um jogador.",
+            "§a/f permissoes §8- §7Utilize para alterar permissões de cargos e membros.",
+            "§a/f promover <jogador> §8- §7Promove um jogador de cargo.",
+            "§a/f proteger §8- §7Utilize para proteger um terreno temporariamente.",
+            "§a/f ranking §8- §7Veja os Rankings com as melhores facções do servidor.",
+            "§a/f rebaixar <jogador> §8- §7Demover um jogador.",
+            "§a/f relacao <facção> §8- §7Gerenciar relação com outras facções.",
+            "§a/f sair §8- §7Utilize para abandonar sua facção.",
+            "§a/f setspawn §8- §7Define a local onde os mobs vão nascer.",
+            "§a/f transferir §8- §7Transfere a liderança da facção para outro jogador.",
+            "§a/f verterras §8- §7Mostrar delimitações de terras.",
+            "§a/f voar §8- §7Habilite ou desabilite voar nos seus terrenos."
+    };
 }
