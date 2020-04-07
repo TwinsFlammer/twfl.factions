81ef130d030aa3739e46e148e7574f79b5cf3f5f
Gutyerrez <srgutyerrez@gmail.com>
2020-04-06 22:37:51 -0300

    Update FactionDisbandCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/argument/FactionDisbandCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/argument/FactionDisbandCommand.java
@@ -1,4 +1,39 @@
 package com.redecommunity.factions.faction.command.argument;
 
-public class FactionDisbandCommand {
+import com.redecommunity.factions.faction.dao.FactionDAO;
+import com.redecommunity.factions.faction.enums.Role;
+import com.redecommunity.factions.faction.manager.FactionManager;
+import com.redecommunity.factions.user.data.FUser;
+import com.redecommunity.factions.util.Messages;
+import org.bukkit.command.CommandSender;
+
+public class FactionDisbandCommand extends AbstractFactionArgumentCommand {
+    public FactionDisbandCommand() {
+        super(
+                0,
+                "desfazer"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        if (!fUser.hasFaction()) {
+            commandSender.sendMessage(Messages.FACTION_NEEDED);
+            return;
+        }
+
+        if (!fUser.isLeader()) {
+            fUser.sendMessage(
+                    String.format(
+                            Messages.ROLE_NEEDED_TO_EXECUTE_THIS_COMMAND,
+                            Role.LEADER.getName()
+                    )
+            );
+            return;
+        }
+
+        // TODO fazer o inventário
+
+        commandSender.sendMessage("Não foi implementado ainda");
+    }
 }
