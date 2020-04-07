e9e57f5ab66fc347810f1963d310193d55bbc393
Gutyerrez <srgutyerrez@gmail.com>
2020-04-06 22:39:13 -0300

    Update FactionLeaveCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/argument/FactionLeaveCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/argument/FactionLeaveCommand.java
@@ -1,4 +1,29 @@
 package com.redecommunity.factions.faction.command.argument;
 
-public class FactionLeaveCommand {
+import com.redecommunity.factions.user.data.FUser;
+import com.redecommunity.factions.util.Messages;
+import org.bukkit.command.CommandSender;
+
+public class FactionLeaveCommand extends AbstractFactionArgumentCommand {
+    public FactionLeaveCommand() {
+        super(
+                0,
+                "sair"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        if (!fUser.hasFaction()) {
+            commandSender.sendMessage(Messages.FACTION_NEEDED);
+            return;
+        }
+
+        if (fUser.isLeader()) {
+            commandSender.sendMessage("§cVocê é o líder da sua facção, transfira ela para alguém antes de sair.");
+            return;
+        }
+
+        // TODO fazer o inventário
+    }
 }
