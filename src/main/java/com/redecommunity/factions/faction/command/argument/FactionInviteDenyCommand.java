f87ce10ef9db08a5fb86325657b482adc3f9a1c9
Gutyerrez <srgutyerrez@gmail.com>
2020-04-06 22:38:41 -0300

    Update FactionInviteDenyCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/argument/FactionInviteDenyCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/argument/FactionInviteDenyCommand.java
@@ -1,4 +1,48 @@
 package com.redecommunity.factions.faction.command.argument;
 
-public class FactionInviteDenyCommand {
+import com.redecommunity.factions.faction.data.Faction;
+import com.redecommunity.factions.faction.manager.FactionManager;
+import com.redecommunity.factions.user.data.FUser;
+import com.redecommunity.factions.util.Messages;
+import org.bukkit.command.CommandSender;
+
+public class FactionInviteDenyCommand extends AbstractFactionArgumentCommand {
+    public FactionInviteDenyCommand() {
+        super(
+                0,
+                "recusar"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        if (args.length == 1) {
+            String tagOrName = args[0];
+
+            Faction faction = FactionManager.getFaction(tagOrName);
+
+            if (faction == null) {
+                commandSender.sendMessage(Messages.FACTION_NOT_EXISTS);
+                return;
+            }
+
+            if (!faction.hasInvited(fUser)) {
+                commandSender.sendMessage("§cEsta facção não enviou um convite para você");
+                return;
+            }
+
+            fUser.getInvites().removeIf(faction1 -> faction1.getId().equals(faction.getId()));
+
+            commandSender.sendMessage(
+                    String.format(
+                            "§aO convite da facção %s foi recusado.",
+                            faction.getName()
+                    )
+            );
+            return;
+        } else {
+            commandSender.sendMessage("§cUtilize /f convite aceitar <tag>.");
+            return;
+        }
+    }
 }
