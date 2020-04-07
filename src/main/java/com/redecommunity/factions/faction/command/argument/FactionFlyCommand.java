6427f9e9fbe5ef95c44cebd5401498c0acc43c8b
Gutyerrez <srgutyerrez@gmail.com>
2020-04-06 22:38:05 -0300

    Update FactionFlyCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/argument/FactionFlyCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/argument/FactionFlyCommand.java
@@ -1,4 +1,50 @@
 package com.redecommunity.factions.faction.command.argument;
 
-public class FactionFlyCommand {
+import com.google.common.collect.Maps;
+import com.redecommunity.common.shared.permissions.group.GroupNames;
+import com.redecommunity.factions.user.dao.FUserDAO;
+import com.redecommunity.factions.user.data.FUser;
+import com.redecommunity.factions.util.Messages;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+
+public class FactionFlyCommand extends AbstractFactionArgumentCommand {
+    public FactionFlyCommand() {
+        super(
+                0,
+                "voar",
+                GroupNames.ELITE
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        if (!fUser.hasFaction()) {
+            commandSender.sendMessage(Messages.FACTION_NEEDED);
+            return;
+        }
+
+        fUser.setFlying(!fUser.isFlying());
+
+        FUserDAO fUserDAO = new FUserDAO();
+
+        HashMap<String, Object> keys = Maps.newHashMap();
+
+        keys.put("flying", fUser.isFlying());
+
+        fUserDAO.update(
+                keys,
+                "user_id",
+                fUser.getId()
+        );
+
+        commandSender.sendMessage(
+                String.format(
+                        "§aO modo voo foi %s.",
+                        fUser.isFlying() ? "ativado" : "desativado"
+                )
+        );
+        return;
+    }
 }
