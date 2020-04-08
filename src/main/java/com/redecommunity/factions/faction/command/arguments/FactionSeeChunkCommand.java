0c9499f06b3b1e2a3c1a1cdd3a8d5ca7f43497fe
Gutyerrez <srgutyerrez@gmail.com>
2020-04-08 15:32:31 -0300

    Update FactionSeeChunkCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/arguments/FactionSeeChunkCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/arguments/FactionSeeChunkCommand.java
@@ -1,4 +1,41 @@
 package com.redecommunity.factions.faction.command.arguments;
 
-public class FactionSeeChunkCommand {
+import com.google.common.collect.Maps;
+import com.redecommunity.factions.user.dao.FUserDAO;
+import com.redecommunity.factions.user.data.FUser;
+import org.bukkit.command.CommandSender;
+
+import java.util.HashMap;
+
+public class FactionSeeChunkCommand extends AbstractFactionArgumentCommand {
+    public FactionSeeChunkCommand() {
+        super(
+                0,
+                "verterras"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        fUser.setSeeingChunks(!fUser.isSeeingChunks());
+
+        FUserDAO fUserDAO = new FUserDAO();
+
+        HashMap<String, Object> keys = Maps.newHashMap();
+
+        keys.put("seeing_chunks", fUser.isSeeingChunks());
+
+        fUserDAO.update(
+                keys,
+                "user_id",
+                fUser.getId()
+        );
+
+        commandSender.sendMessage(
+                String.format(
+                        "§aModo ver terras foi %s.",
+                        fUser.isSeeingChunks() ? "ativado" : "desativado"
+                )
+        );
+    }
 }
