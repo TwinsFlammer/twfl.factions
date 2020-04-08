189fc4b8e590b7ae687b5d3257ccbf5d44220851
Gutyerrez <srgutyerrez@gmail.com>
2020-04-08 00:23:44 -0300

    Update FactionInviteDenyCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/arguments/FactionInviteDenyCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/arguments/FactionInviteDenyCommand.java
@@ -1,4 +1,4 @@
-package com.redecommunity.factions.faction.command.argument;
+package com.redecommunity.factions.faction.command.arguments;
 
 import com.redecommunity.factions.faction.data.Faction;
 import com.redecommunity.factions.faction.manager.FactionManager;
