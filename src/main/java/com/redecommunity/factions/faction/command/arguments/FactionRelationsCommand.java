36b850cb9df017344d40df5ddca06830584cc32b
Gutyerrez <srgutyerrez@gmail.com>
2020-04-08 15:32:09 -0300

    Update FactionRelationsCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/arguments/FactionRelationsCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/arguments/FactionRelationsCommand.java
@@ -1,4 +1,18 @@
 package com.redecommunity.factions.faction.command.arguments;
 
-public class FactionRelationsCommand {
+import com.redecommunity.factions.user.data.FUser;
+import org.bukkit.command.CommandSender;
+
+public class FactionRelationsCommand extends AbstractFactionArgumentCommand {
+    public FactionRelationsCommand() {
+        super(
+                0,
+                "relacoes"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        // TODO not implemented yet
+    }
 }
