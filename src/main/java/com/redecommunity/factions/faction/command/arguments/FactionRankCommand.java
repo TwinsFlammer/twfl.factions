b3ab16a40ecaebb4f833ed7bcde6d83664d8a269
Gutyerrez <srgutyerrez@gmail.com>
2020-04-08 15:31:38 -0300

    Update FactionRankCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/arguments/FactionRankCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/arguments/FactionRankCommand.java
@@ -1,4 +1,18 @@
 package com.redecommunity.factions.faction.command.arguments;
 
-public class FactionRankCommand {
+import com.redecommunity.factions.user.data.FUser;
+import org.bukkit.command.CommandSender;
+
+public class FactionRankCommand extends AbstractFactionArgumentCommand {
+    public FactionRankCommand() {
+        super(
+                0,
+                "ranking"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        // TODO not implemented yet
+    }
 }
