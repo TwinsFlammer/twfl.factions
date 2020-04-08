ffd40470f9dabdc06ccfa131d9542e48e8c1bf70
Gutyerrez <srgutyerrez@gmail.com>
2020-04-08 15:31:03 -0300

    Update FactionInvitesCommand.java

--- src/main/java/com/redecommunity/factions/faction/command/arguments/FactionInvitesCommand.java
+++ src/main/java/com/redecommunity/factions/faction/command/arguments/FactionInvitesCommand.java
@@ -1,4 +1,18 @@
 package com.redecommunity.factions.faction.command.arguments;
 
-public class FactionInvitesCommand {
+import com.redecommunity.factions.user.data.FUser;
+import org.bukkit.command.CommandSender;
+
+public class FactionInvitesCommand extends AbstractFactionArgumentCommand {
+    public FactionInvitesCommand() {
+        super(
+                0,
+                "convites"
+        );
+    }
+
+    @Override
+    public void onCommand(CommandSender commandSender, FUser fUser, String[] args) {
+        // TODO not implemented yet
+    }
 }
